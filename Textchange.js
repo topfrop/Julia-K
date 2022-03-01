91e9b9c3c625dead7284f366c01a31edc685763c
topfrop <97982603+topfrop@users.noreply.github.com>
2022-03-01 15:01:43 -0500

    Add files via upload

---
+++ Textchange.js
@@ -0,0 +1,49 @@
+let replaceTextINTRO = (e) => {
+  $("#element-intro").text("You’re lucky I agreed to this! I’ve got an entire Kingdom to run, you know, let’s make this snappy.");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-intro', replaceTextINTRO);
+
+
+let replaceText1 = (e) => {
+  $("#element-1").text("Mario... The second I see his face, Mr. Nice Bowser is GONE. He’s always getting in the way of me and Peach. I’ll take any chance I can get to show that mustachioed loser who’s boss, even on our days off. No one is better at golf than me!");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-1', replaceText1);
+
+let replaceText2 = (e) => {
+  $("#element-2").text("My pleasure! They don’t call me the Koopa King for nothing. Did you know I’ve carried my entire castle before? I’ve also gone through a black hole like it was nothing, no sweat! Oh and I can’t forget to mention how I’ve swam through super hot lava! Hey, are you writing this down?");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-2', replaceText2);
+
+let replaceText3 = (e) => {
+  $("#element-3").text("Oh you don’t know the half of it! All of my minions are sooo incompetent, it’s like I have to do everything! How am I supposed to make Junior into the next greatest leader if I gotta keep babysitting my troops? Speaking of Junior, if you’re listening to this, IT’S PAST YOUR BEDTIME!");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-3', replaceText3);
+
+let replaceText4 = (e) => {
+  $("#element-4").text("GWAHAHAHA! You’re funny.");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-4', replaceText4);
+
+let replaceText5 = (e) => {
+  $("#element-5").text("What! You little punk, where did you find those? No comment!");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-5', replaceText5);
+
+let replaceText6 = (e) => {
+  $("#element-6").text("Why? Because I want it! Duh! Peach is standing in my way of taking the Mushroom Kingdom for my own, so I gotta kidnap her to keep her out of the way. That, and I think Junior needs a mother figure.");
+};
+
+// Attaching the click event on the button
+$(document).on('click', '#replaceText-6', replaceText6);
